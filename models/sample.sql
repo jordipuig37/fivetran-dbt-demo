5a9848ba5702eaf33b3e3f7cc4ba7995ae21600d
Jordi Puig <jpuig@t2client.com>
2023-03-21 17:34:00 +0100

    Changed the select for a simpler one

--- models/sample.sql
+++
@@ -1,8 +0,0 @@
-select weather_conditions as conditions,
-       count(*) as num_trips
-from FIVETRAN_DATABASE.S3_DEMO.TRIPS_DEMO
-left outer join FIVETRAN_DATABASE.S3_DEMO.WEATHER_NYC
-    on date_trunc('hour', observation_time) = date_trunc('hour', starttime)
-where conditions is not null
-group by 1
-order by 2 desc;
